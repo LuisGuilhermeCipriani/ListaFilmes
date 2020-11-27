8267632ef50a75a71a52b277eb926f868621cf88
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-26 21:11:10 -0300

    Adiciona recyclerViewFilmes

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/filmes/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/filmes/MainActivity.java
@@ -1,14 +1,18 @@
 package br.ufjf.dcc196.luisguilhermecipriani.filmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewFilmes =findViewById(R.id.recyclerViewFilmes);
     }
 }
