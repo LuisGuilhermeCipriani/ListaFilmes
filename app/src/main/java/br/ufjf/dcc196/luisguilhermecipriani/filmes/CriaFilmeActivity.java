cb59c3866cf6acda87b2117fcff861b1418fcb31
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-27 12:54:03 -0300

    Cria componentes para CriaFilmeActivity

---
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/filmes/CriaFilmeActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.luisguilhermecipriani.filmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriaFilmeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cria_filme);
+    }
+}
