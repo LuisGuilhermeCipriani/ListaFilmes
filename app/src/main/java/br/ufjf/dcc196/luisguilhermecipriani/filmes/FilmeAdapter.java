f3c84cc382ef22f4df1a0f00b3ee6e35627ff203
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-27 10:09:39 -0300

    Altera adapter para mostrar os dados

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/filmes/FilmeAdapter.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/filmes/FilmeAdapter.java
@@ -1,7 +1,10 @@
 package br.ufjf.dcc196.luisguilhermecipriani.filmes;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,22 +22,44 @@ public class FilmeAdapter extends RecyclerView.Adapter<FilmeAdapter.FilmeViewHol
     @NonNull
     @Override
     public FilmeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context ctx = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View filmeView = inflater.inflate(R.layout.filme_layout, parent, false);
+        FilmeViewHolder viewHolder = new FilmeViewHolder(filmeView);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull FilmeViewHolder holder, int position) {
+        String verificaAssistido = "Não Assistido";
+        if(filmes.get(position).getAssistido().toString() == "true")
+            verificaAssistido = "Assistido";
 
+        holder.textViewTitulo.setText(filmes.get(position).getTitulo());
+        holder.textViewAno.setText(filmes.get(position).getAno());
+        holder.textViewEstilo.setText(filmes.get(position).getEstilo());
+        holder.textViewDiretor.setText(filmes.get(position).getDiretor());
+        holder.textViewAssistido.setText(verificaAssistido);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filmes.size();
     }
 
     public class FilmeViewHolder extends RecyclerView.ViewHolder{
+        private TextView textViewTitulo;
+        private TextView textViewAno;
+        private TextView textViewEstilo;
+        private TextView textViewDiretor;
+        private TextView textViewAssistido;
         public FilmeViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewTitulo = itemView.findViewById(R.id.textViewTitulo);
+            textViewAno = itemView.findViewById(R.id.textViewAno);
+            textViewEstilo = itemView.findViewById(R.id.textViewEstilo);
+            textViewDiretor = itemView.findViewById(R.id.textViewDiretor);
+            textViewAssistido = itemView.findViewById(R.id.textViewAssistido);
         }
     }
 }
